778be68a0e9e18fdab791d2f4539d4a018cd67dc
Brenda Velasquez <brenda.velasquez99@gmail.com>
2019-01-06 22:36:41 -0700

    Updates

---
+++ precache-manifest.6c2b1f38a70877ff28224e2b1ec392d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9ffd6160dde2f7bf7cc2b5849b59f22",
+    "url": "/bloc-jams-react/static/media/bloc jams background photo.a9ffd616.jpg"
+  },
+  {
+    "revision": "8532c21bd7bbf1dd1b0c",
+    "url": "/bloc-jams-react/static/js/runtime~main.8532c21b.js"
+  },
+  {
+    "revision": "f90665b0310d52d4724a",
+    "url": "/bloc-jams-react/static/js/main.f90665b0.chunk.js"
+  },
+  {
+    "revision": "f08169d20ff18b7b009a",
+    "url": "/bloc-jams-react/static/js/1.f08169d2.chunk.js"
+  },
+  {
+    "revision": "f90665b0310d52d4724a",
+    "url": "/bloc-jams-react/static/css/main.891d5fbf.chunk.css"
+  },
+  {
+    "revision": "28664df4b4de9ce05c1effc260729923",
+    "url": "/bloc-jams-react/index.html"
+  }
+];
