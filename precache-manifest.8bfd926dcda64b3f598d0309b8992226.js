43c16d8cf42525c52b2e6fd0c7b4c920fb50c34f
Brenda Velasquez <brenda.velasquez99@gmail.com>
2019-01-07 20:50:44 -0700

    Updates

---
+++ precache-manifest.8bfd926dcda64b3f598d0309b8992226.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9ffd6160dde2f7bf7cc2b5849b59f22",
+    "url": "/bloc-jams-react/static/media/bloc jams background photo.a9ffd616.jpg"
+  },
+  {
+    "revision": "8532c21bd7bbf1dd1b0c",
+    "url": "/bloc-jams-react/static/js/runtime~main.8532c21b.js"
+  },
+  {
+    "revision": "5566fd3d5b4d3a114188",
+    "url": "/bloc-jams-react/static/js/main.5566fd3d.chunk.js"
+  },
+  {
+    "revision": "f08169d20ff18b7b009a",
+    "url": "/bloc-jams-react/static/js/1.f08169d2.chunk.js"
+  },
+  {
+    "revision": "5566fd3d5b4d3a114188",
+    "url": "/bloc-jams-react/static/css/main.0b2ac1e5.chunk.css"
+  },
+  {
+    "revision": "0a378881761ae801905430ea1ca77044",
+    "url": "/bloc-jams-react/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f5a01b63a75f3d7c68a5ee27b83befbd.js"
+  "/bloc-jams-react/precache-manifest.8bfd926dcda64b3f598d0309b8992226.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/bloc-jams-react/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
